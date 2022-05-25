959f270b539ccdcaea5913f71daf9a060902ce8d
Luka Sever <lukkalukaa@gmail.com>
2022-05-25 10:41:07 +0200

    Reworked the SQL querry and Inputs

---
+++ database/manager.js
@@ -0,0 +1,41 @@
+const Server = null
+
+module.exports = class Manager {
+  static async createServer(id, connection) {
+    var sql =
+      "INSERT INTO Servers (id, prefix) VALUES ("+"'"+id+"'"+", NULL)";
+    connection.query(sql, function (err, result) {
+      if (err) console.log(err);
+      else console.log("1 record inserted");
+    });
+    // const result = new Server({
+    //   serverId: id,
+    //   prefix: null,
+    // });
+
+    // await result.save();
+
+    // return result;
+  }
+  static async findServer(id) {
+    const result = await Server.findOne({ serverId: id });
+
+    return result;
+  }
+
+  static async getPrefix(id) {
+    const result = await Server.findOne({ serverId: id });
+
+    return result.prefix;
+  }
+
+  static async updateServerPrefix(id, prefix) {
+    const result = await Server.findOne({ serverId: id });
+
+    if (result) {
+      return await result.updateOne({ prefix });
+    } else {
+      return;
+    }
+  }
+};
