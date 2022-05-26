e3acd068deaa2a6a1c5d3580541d7ec0067973d3
Luka Sever <lukkalukaa@gmail.com>
2022-05-26 20:29:44 +0200

    Fixing git

---
+++ commands.js
@@ -0,0 +1,54 @@
+const { SlashCommandBuilder, ContextMenuCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const config = require("./config.json");
+
+const deploy = async () => {
+	const commands = [
+		new SlashCommandBuilder()
+			.setName('ping')
+			.setDescription('Returns the ping.'),
+		new SlashCommandBuilder()
+			.setName('prefix')
+			.setDescription('Manages the bot prefix.')
+			.addSubcommand((sc) =>
+				sc
+					.setName('view')
+					.setDescription('Views the current prefix.'),
+			)
+			.addSubcommand((sc) =>
+				sc
+					.setName('set')
+					.setDescription('Sets the prefix to a new one.')
+					.addStringOption((string) =>
+						string
+							.setName('value')
+							.setDescription('The new prefix.')
+							.setRequired(true),
+					),
+			),
+		new ContextMenuCommandBuilder()
+			.setName('User Info')
+			.setType(2),
+	].map(cmd => cmd.toJSON());
+
+	const rest = new REST({ version: '9' }).setToken(config.token);
+
+	try {
+		const clientId = config.client_id;
+
+		console.log('Started refreshing Slash Commands and Context Menus...');
+
+		await rest.put(
+			Routes.applicationCommands(clientId),
+			{ body: commands },
+		).then(() => {
+			console.log('Slash Commands and Context Menus have now been deployed.');
+		});
+	}
+	catch (e) {
+		console.error(e);
+	}
+};
+
+deploy();
